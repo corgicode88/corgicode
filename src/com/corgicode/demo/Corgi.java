08b916edb90921f318576241eacd4286e6b019d8
Corgicode <corgicode88@gmail.com>
2023-08-30 15:44:53 +0700

    update corgi

--- src/com/corgicode/demo/Corgi.java
+++ src/com/corgicode/demo/Corgi.java
@@ -1,14 +1,11 @@
 package com.corgicode.demo;
 
-// A class that represents a corgi object
 class Corgi {
-    // Attributes of a corgi
     private String name; // the name of the corgi
     private int energy; // the energy level of the corgi, from 0 to 100
     private int mood; // the mood of the corgi, from -50 to 50
     private boolean isPlaying; // whether the corgi is playing fetch or not
 
-    // Constructor of a corgi
     public Corgi(String name) {
         this.name = name;
         this.energy = 100;
@@ -16,29 +13,22 @@ class Corgi {
         this.isPlaying = false;
     }
 
-// Methods of a corgi
-
-    // A method that returns the name of the corgi
     public String getName() {
         return this.name;
     }
 
-    // A method that returns the energy level of the corgi
     public int getEnergy() {
         return this.energy;
     }
 
-    // A method that returns the mood of the corgi
     public int getMood() {
         return this.mood;
     }
 
-    // A method that returns whether the corgi is playing fetch or not
     public boolean isPlaying() {
         return this.isPlaying;
     }
 
-    // A method that sets the energy level of the corgi
     public void setEnergy(int energy) {
         this.energy = energy;
         if (this.energy < 0) {
@@ -49,7 +39,6 @@ class Corgi {
         }
     }
 
-    // A method that sets the mood of the corgi
     public void setMood(int mood) {
         this.mood = mood;
         if (this.mood < -50) {
@@ -60,26 +49,22 @@ class Corgi {
         }
     }
 
-    // A method that sets whether the corgi is playing fetch or not
     public void setPlaying(boolean isPlaying) {
         this.isPlaying = isPlaying;
     }
 
-    // A method that makes the corgi bark
     public void bark() {
         System.out.println("Woof woof!");
         this.setEnergy(this.getEnergy() - 10); // barking reduces energy by 10
         this.setMood(this.getMood() + 10); // barking increases mood by 10
     }
 
-    // A method that makes the corgi wag its tail
     public void wagTail() {
         System.out.println("Wag wag wag!");
         this.setEnergy(this.getEnergy() - 5); // wagging tail reduces energy by 5
         this.setMood(this.getMood() + 5); // wagging tail increases mood by 5
     }
 
-    // A method that makes the corgi play fetch with a ball
     public void playFetch(Ball ball) {
         System.out.println("Fetch!");
         ball.throwBall(); // throw the ball
@@ -88,7 +73,6 @@ class Corgi {
         this.setMood(this.getMood() + 20); // playing fetch increases mood by 20
     }
 
-    // A method that makes the corgi do a trick
     public void doTrick(String trick) {
         System.out.println("Trick: " + trick);
         switch (trick) { // different tricks have different effects on energy and mood
@@ -112,5 +96,15 @@ class Corgi {
                 break;
         }
 
+    }
+
+    @Override
+    public String toString() {
+        return "Corgi{" +
+                "name='" + name + '\'' +
+                ", energy=" + energy +
+                ", mood=" + mood +
+                ", isPlaying=" + isPlaying +
+                '}';
     }
 }
--- src/com/corgicode/demo/CorgiGUI.java
+++ src/com/corgicode/demo/CorgiGUI.java
@@ -20,15 +20,13 @@ class CorgiGUI extends JFrame implements ActionListener {
     private JTextArea outputTextArea; // a text area to display the output of the simulation
 
     // Attributes of the GUI
-    private Corgi corgi; // a corgi object to simulate
-    private Ball ball; // a ball object to play fetch with
+    private static Corgi corgi; // a corgi object to simulate
+    private static Ball ball; // a ball object to play fetch with
 
     // Constructor of the GUI
     public CorgiGUI(Corgi corgi, Ball ball) {
         this.corgi = corgi;
         this.ball = ball;
-
-// Initialize the components
         nameLabel = new JLabel("Name: " + corgi.getName());
         energyLabel = new JLabel("Energy: " + corgi.getEnergy());
         moodLabel = new JLabel("Mood: " + corgi.getMood());
@@ -38,11 +36,7 @@ class CorgiGUI extends JFrame implements ActionListener {
         doTrickButton = new JButton("Do Trick");
         trickComboBox = new JComboBox<String>(new String[]{"sit", "roll over", "shake paw", "speak"});
         outputTextArea = new JTextArea(10, 40);
-
-// Set the layout of the GUI
         setLayout(new BorderLayout());
-
-// Add the components to the GUI
         JPanel topPanel = new JPanel(); // a panel for the top part of the GUI
         topPanel.add(nameLabel);
         topPanel.add(energyLabel);
@@ -59,60 +53,47 @@ class CorgiGUI extends JFrame implements ActionListener {
 
         JScrollPane scrollPane = new JScrollPane(outputTextArea); // a scroll pane for the text area
         add(scrollPane, BorderLayout.SOUTH);
-
-// Add action listeners to the buttons
         barkButton.addActionListener(this);
         wagTailButton.addActionListener(this);
         playFetchButton.addActionListener(this);
         doTrickButton.addActionListener(this);
-
-// Set the title and size of the GUI
         setTitle("Corgi Simulator");
         setSize(500, 400);
-
-// Set the GUI to be visible and closeable
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     // A method that handles the actions performed on the GUI
     public void actionPerformed(ActionEvent e) {
-
+        Corgi corgiU = this.corgi;
         if (e.getSource() == barkButton) { // if the bark button is clicked
-            corgi.bark(); // make the corgi bark
-            updateGUI(); // update the GUI labels and text area
+            corgiU.bark(); // make the corgi bark
+            updateGUI(corgiU); // update the GUI labels and text area
         }
 
         if (e.getSource() == wagTailButton) { // if the wag tail button is clicked
             corgi.wagTail(); // make the corgi wag its tail
-            updateGUI(); // update the GUI labels and text area
+            updateGUI(corgi); // update the GUI labels and text area
         }
 
         if (e.getSource() == playFetchButton) { // if the play fetch button is clicked
             corgi.playFetch(ball); // make the corgi play fetch with the ball
-            updateGUI(); // update the GUI labels and text area
+            updateGUI(corgi); // update the GUI labels and text area
         }
 
         if (e.getSource() == doTrickButton) { // if the do trick button is clicked
             String trick = (String) trickComboBox.getSelectedItem(); // get the selected trick from the combo box
             corgi.doTrick(trick); // make the corgi do the trick
-            updateGUI(); // update the GUI labels and text area
+            updateGUI(corgi); // update the GUI labels and text area
         }
 
     }
 
-    // A method that updates the GUI labels and text area
-    public void updateGUI() {
-// Update the energy label with the current energy level of the corgi
+    public void updateGUI(Corgi corgi) {
         energyLabel.setText("Energy: " + corgi.getEnergy());
-
-// Update the mood label with the current mood of the corgi
         moodLabel.setText("Mood: " + corgi.getMood());
-
-// Append the output of the simulation to the text area
-        outputTextArea.append(corgi.getName() + ": " + System.console().readLine() + "\n");
-
-// Scroll to the bottom of the text area
+        outputTextArea.append(corgi.getName() + ":" +corgi.toString() + "\n");
+        //outputTextArea.append(corgi.getName() + ": " + System.console().readLine() + "\n");
         outputTextArea.setCaretPosition(outputTextArea.getDocument().getLength());
     }
 }
